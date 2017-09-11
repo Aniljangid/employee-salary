fa610a7dd7b1b32051047692053c20b99970a6b4
Adarsh <adarshbhalotia2910@gmail.com>
2017-09-11 18:54:51 +0530

    Added server.js

---
+++ server.js
@@ -0,0 +1,106 @@
+//Author : Adarsh
+//Date Created : 28 July 2017
+var express = require('express');
+var app = express();
+var mysql = require('mysql');
+var bodyParser = require('body-parser');
+var path = require('path');
+var session = require('express-session');
+
+var conn = mysql.createConnection({
+	host : 'localhost',
+	user : 'root',
+	password : '',
+	db : 'empsalmgt'
+})
+
+conn.connect(err) {
+	if(!err) {
+		console.log("Connection established...");
+	}
+	else {
+		console.log(err);
+		console.log("Connection failed...");
+	}
+}
+
+app.use(session({
+  cookie: {
+    path    : '/',
+    httpOnly: false
+  },
+  	secret: 'anil',
+	resave: false,
+	saveUninitialized: false
+}));
+
+app.use(express.static('Resorces'));
+
+
+function noCache(req, res, next) {
+  res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+  res.header('Expires', '-1');
+  res.header('Pragma', 'no-cache');
+  next();
+}
+
+function checkAuth(req, res, next) {
+ 	if (!req.session.admin_password) {
+    	res.send('Not Authorized');
+  	} else {
+  		next();
+  	}
+}
+
+app.get('/',noCache,function(req,res){
+	if(!req.session.admin_password) {
+		res.sendFile(path.join(__dirname + '/login.html'));
+	}
+	else{
+		res.redirect('/dashboard')
+	}
+});
+
+
+app.get('/logout', function (req, res) {
+  delete req.session.regNo;
+  res.send({redirect:'/',result:'logout'});
+}); 
+
+app.get('/dashboard', checkAuth, function(req,res) {
+	console.log(req.session.admin_password);
+	req.session.dontGoBack = "true";
+  	res.sendFile(path.join(__dirname + '/dashboard.html'));
+});
+
+app.post('/login', function(req,res) {
+	var password = req.body.password;
+	if(!req.session.admin_password) {
+		conn.query('SELECT * FROM admin WHERE admin_password = ?',[password], function(error,response) {
+			if(!error){
+				if(response[0]) {
+					console.log("success");
+						req.session.admin_password = password;
+						res.send({redirect:'/dashboard',result:'success'});
+					}
+					else {
+						res.send({redirect:'/',result:'invalid'});
+						console.log("Incorrect password");
+					}
+				}
+				else
+					console.log(error);
+				});
+			}
+	else {
+		console.log("Invalid password");
+		res.send({redirect:'/',result:'invalid'}); 
+	}
+	else
+		console.log('error');
+});
+
+app.listen(5555,function(){
+	console.log("Server running at port 5555");
+})
+
