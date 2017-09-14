66ef23bff13acc1147654210265863a897ca65f7
Abid Hussain <abid.abid97@gmail.com>
2017-09-14 18:49:54 +0530

    added save ajax call

--- static/Dashboard.js
+++ static/Dashboard.js
@@ -10,6 +10,27 @@ $(document).ready(function(){
       window.location = res.redirect;
     })
   })
+  
+  $('#saveemp').click(function(){
+    var FormData = {
+      empId: $('#empid').val(),
+      empName: $('#empname').val(),
+      phone: $('#empphnum').val(),
+      basicPay: $('#empbasic').val()
+    };
+    
+    $.ajax({
+      type: 'POST',
+      url: 'http://192.168.43.31:5555/insert',
+      data: FormData,
+      datatype: 'json',
+      encode: true
+    }).done(function(res){
+      console.log(res);
+      console.log("insert into into database");
+    })
+  })
+  
 })
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
