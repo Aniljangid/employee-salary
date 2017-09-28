3caeb196b7802dd16cb2d8c897fad99872a037cf
Abid Hussain <abid.abid97@gmail.com>
2017-09-29 01:29:29 +0530

    updated UI for employee dashboard

--- static/EmpDashboard.js
+++ static/EmpDashboard.js
@@ -1,17 +1,28 @@
 $(document).ready(function(){
+  var width = $(".infobox").width();
+  $('.infobox').height(width);
+  $('.cardbar').height(width * 18 / 100);
+  $('.cardbody').height(width * 82 / 100);
+  
   $.ajax({
     type: 'GET',
     url: 'http://localhost:5555/getDetails',
     encode: true
   }).done(function(res){
-    console.log(res.result[0].id);
-    $('.table-body').append('<tr class="tablerow' + 0 + ' tablerow"></tr>')
-    $('.tablerow' + 0).append('<td>' + res.result[0].id + '</td>')
-    $('.tablerow' + 0).append('<td>' + res.result[0].name + '</td>')
-    $('.tablerow' + 0).append('<td>' + res.result[0].att + '</td>')
-    $('.tablerow' + 0).append('<td>' + res.result[0].basicpay + '</td>')
-    $('.tablerow' + 0).append('<td>' + res.result[0].adv + '</td>')
-    $('.tablerow' + 0).append('<td>' + res.result[0].totalsal + '</td>')
+    // console.log(res.result[0].id);
+    // $('.table-body').append('<tr class="tablerow' + 0 + ' tablerow"></tr>')
+    // $('.tablerow' + 0).append('<td>' + res.result[0].id + '</td>')
+    // $('.tablerow' + 0).append('<td>' + res.result[0].name + '</td>')
+    // $('.tablerow' + 0).append('<td>' + res.result[0].att + '</td>')
+    // $('.tablerow' + 0).append('<td>' + res.result[0].basicpay + '</td>')
+    // $('.tablerow' + 0).append('<td>' + res.result[0].adv + '</td>')
+    // $('.tablerow' + 0).append('<td>' + res.result[0].totalsal + '</td>')
+    
+    $('.cardbody-name').append('<h1 class="cardbody-text">' + res.result[0].id + '</h1>')
+    $('.cardbody-att').append('<h1 class="cardbody-text">' + res.result[0].att + '</h1>')
+    $('.cardbody-adv').append('<h1 class="cardbody-text">' + res.result[0].adv + '</h1>')
+    $('.cardbody-basicpay').append('<h1 class="cardbody-text">' + res.result[0].basicpay + '</h1>')
+    $('.cardbody-total').append('<h1 class="cardbody-text">' + res.result[0].totalsal + '</h1>')
   })
   
   $('.logoutbtn').click(function(){
@@ -22,6 +33,12 @@ $(document).ready(function(){
     }).done(function(res){
       window.location = res.redirect;
     })
-  })
-  
+  })  
+})
+
+$(window).resize(function(){
+  var width = $(".infobox").width();
+  $('.infobox').height(width);
+  $('.cardbar').height(width * 18 / 100);
+  $('.cardbody').height(width * 82 / 100);
 })
