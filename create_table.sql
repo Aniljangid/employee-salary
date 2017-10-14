d0afa77eba50b91573876561fd062913a07d4171
Abid Hussain <abid.abid97@gmail.com>
2017-10-14 14:59:37 +0530

    front end company name

--- create_table.sql
+++ create_table.sql
@@ -1,6 +1,6 @@
 create database empsalmgt;
 use empsalmgt;
-create table admin(admin_password varchar(16));
+create table admin(admin_company varchar(16), admin_password varchar(16));
 -- insert into admin values('itspassword');
 
-create table empdetails(id int, name varchar(24), password varchar(24) ,phnum varchar(10), basicpay int, adv int, att int, totalsal int, PRIMARY KEY (id));
+create table empdetails(id varchar(24), name varchar(24), password varchar(24) ,phnum varchar(10), companyName varchar(16), basicpay int, adv int, att int, totalsal int, PRIMARY KEY (id));
--- static/login.js
+++ static/login.js
@@ -20,12 +20,14 @@ $(function() {
 $(document).ready(function(){  //loads when the page is loaded
   $("#admin-submit").click(function(e){ // executes the function when clicked on admin-submit button
     e.preventDefault();
+    var companyName = $('#admin_password').val()
     var password = $('#admin_password').val() // fethes the value of password from textbox
-
     $.ajax({          // ajax call is used to send data to backend (php)
       type: 'POST',   // type of request (POST or GET)
       url: 'http://localhost:5555/login', // where do you want to send your data
-      data: { password: password },   //what type of data is it
+      data: { password: password ,
+              companyName: companyName
+      },   //what type of data is it
       encode: true
     }).done(function(res) {   //done function is executed after the php code is done executing
       if(res.result=="success") {
